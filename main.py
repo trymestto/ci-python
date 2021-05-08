e711371bc954be132c3104dd3d583c9a25080e54
trymestto <58995540+trymestto@users.noreply.github.com>
2021-05-08 15:29:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+  
 import os
 import math
 
 
-def func1(a, b) -> str:
-  return math.floor(a + b)
-  
-  
-def func2(a, b, c) -> int:
-  return os.getcwd()
+def func1(a, b) -> int:
+    return math.floor(a + b)
+
+
+def func2(a, b, c) -> str:
+    return os.getcwd()
